522e47a52dc8ae1a0a5736350034c93bb39b4446
LX <lxiao723@163.com>
2015-05-19 20:39:06 +0800

    添加Xib测试

---
+++ LXTest/LXTest/Class/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  LXTest
+//
+//  Created by 李晓 on 15/5/19.
+//  Copyright (c) 2015年 teradata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
